ea17328d3247f11fe82df09416ce1efb0f17bf00
somov.a <somov.lesha51@gmail.com>
2023-06-21 13:51:32 +0300

    wip: repository for future work with orm

--- src/main/java/com/sopline/urlshorter/repository/RepositoryUrl.java
+++ src/main/java/com/sopline/urlshorter/repository/RepositoryUrl.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RepositoryEncode extends JpaRepository<EntityUrl, Long> {
-
-    EntityUrl findById(EntityUrl entityUrl);
+public interface RepositoryUrl extends JpaRepository<EntityUrl, String> {
 }

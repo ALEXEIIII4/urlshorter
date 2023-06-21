9b61bb06a2901ed365a4ce037ecb871a3befba95
somov.a <somov.lesha51@gmail.com>
2023-06-21 13:50:56 +0300

    add: URL cut

---
+++ src/main/java/com/sopline/urlshorter/service/ServiceUrl.java
@@ -0,0 +1,46 @@
+package com.sopline.urlshorter.service;
+
+import com.sopline.urlshorter.repository.RepositoryUrl;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class ServiceUrl {
+
+    private final RepositoryUrl repositoryUrl;
+
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int BASE = ALPHABET.length();
+    // длина короткого URL-адреса
+    private static final int LENGTH = 7;
+
+    static public String cutHost(String shouldBeShort) {
+        String host = null;
+        int count = 0;
+        for (int i = 0; i < shouldBeShort.length(); i++) {
+            if (shouldBeShort.charAt(i) == '/')
+                count++;
+            if (count == 3) {
+                host = shouldBeShort.substring(0, i + 1);
+                break;
+            }
+        }
+        return host;
+    }
+
+
+    public String makeShortUrl(String longUrl) {
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+        String domain = cutHost(longUrl);
+        for (int i = 0; i < LENGTH; i++) {
+            int index = random.nextInt(BASE);
+            sb.append(ALPHABET.charAt(index));
+        }
+        return domain + sb.toString();
+    }
+
+}

a4de0a3f8e9886e53afc4289052f9849a0720fe6
somov.a <somov.lesha51@gmail.com>
2023-06-21 13:49:59 +0300

    change: entity fields

--- src/main/java/com/sopline/urlshorter/entity/EntityUrl.java
+++ src/main/java/com/sopline/urlshorter/entity/EntityUrl.java
@@ -10,18 +10,18 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode(of = {"id"})
-@ToString(of = {"id"})
 public class EntityUrl {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String shortUrl;
+    private String token;
 
     private String longUrl;
 
-    private LocalDateTime timestamp;
+    private LocalDateTime createdAt;
+
+    private LocalDateTime expiryAt;
 
 }

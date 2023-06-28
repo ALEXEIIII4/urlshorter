83ed57508cc59b62e10aa53360f5ef7d970e52a1
somov.a <somov.lesha51@gmail.com>
2023-06-28 19:25:20 +0300

    wip: repo for work

--- src/main/java/com/sopline/urlshorter/repository/UrlShortenerRepository.java
+++ src/main/java/com/sopline/urlshorter/repository/UrlShortenerRepository.java
@@ -3,7 +3,9 @@ package com.sopline.urlshorter.repository;
 import com.sopline.urlshorter.entity.UrlShortenerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.Optional;
 @Repository
-public interface UrlShortenerRepository extends JpaRepository<UrlShortenerEntity, String> {
+public interface UrlShortenerRepository extends JpaRepository<UrlShortenerEntity, Long> {
+    Optional<UrlShortenerEntity> findById(Long id);
+    Optional<UrlShortenerEntity> findByToken(String url);
 }
